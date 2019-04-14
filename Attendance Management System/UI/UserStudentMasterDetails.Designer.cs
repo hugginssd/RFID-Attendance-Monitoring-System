7a00b4ee63ef3a128d23492c74f4ff01a853f95a
hugginssd <hugginsmafigu@gmail.com>
2019-04-14 20:56:00 +0200

    Changes student master details UI

--- Attendance Management System/UI/UserStudentMasterDetails.Designer.cs
+++ Attendance Management System/UI/UserStudentMasterDetails.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UserStudentMasterDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UserStudentMasterDetails";
+            this.Size = new System.Drawing.Size(912, 346);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
